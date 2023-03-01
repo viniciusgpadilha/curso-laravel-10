c16c4a8a0fcc5d52312873892bc373094688332b
viniciusgpadilha <vp2002@gmail.com>
2023-03-01 16:30:04 -0300

    Estudo sobre DB

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::factory()->count(10)->create();
+
+        // User::create([
+        //     'name' => 'Carlos',
+        //     'email' => 'carlos@gmail.com',
+        //     'password' => bcrypt('123456'), 
+        // ]);
+    }
+}
